cc1ce78e17f1556e98923b5482f2c39eb4fd946d
Joachim Brolin <jbrolin90@gmail.com>
2026-06-03 16:25:39 +0200

    refactor: remove RepetitionDetector from task package exports
    RepetitionDetector is now an internal detail of ConversationHistory

--- task/__init__.py
+++ task/__init__.py
@@ -1,6 +1,5 @@
 """Task package - orchestrates agent task execution."""
 from task.task import Task
-from task.repetition_detector import RepetitionDetector
 from task.tool_engine import ToolEngine
 
-__all__ = ["Task", "RepetitionDetector", "ToolEngine"]
+__all__ = ["Task", "ToolEngine"]
